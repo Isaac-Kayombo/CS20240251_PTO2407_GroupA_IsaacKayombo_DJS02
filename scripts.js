9b86f6616ab4d32d569c4ec72a60e7422bb6ac84
IsaacK47 <isaackayombo41@gmail.com>
2025-03-21 20:33:58 +0200

    Logic checking if inputs are empty.

--- scripts.js
+++ scripts.js
@@ -5,7 +5,13 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  
+
+  // CHECKS IF EITHER OF THE INPUTS ARE EMPTY
+  if (!dividend || !divider) {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    return;
+  }
+
   // PERFORMS CALCULATION AND ROUNDS RESULT TO THE NEAREST WHOLE NUMBER
   const finalValue = Math.floor(dividend / divider);
   result.innerText = finalValue;
